8de230e366f5270ed5881731bd1ecc88cd99bd0b
Anil Singh <anillibra76@gmail.com>
2017-07-23 00:18:48 +0530

    commit

--- first.ts
+++ first.ts
@@ -1,17 +1,8 @@
-<<<<<<< HEAD
 1 devUpdated line
 2 dev line
 3 dev line
-=======
-1 dev line
-2 dev line
->>>>>>> a950f5f95eb04d3ffb35b02c02ffe9925b4a950c
 
 1 feature line
 2 feature line
 3 feature line
-<<<<<<< HEAD
 4 feature line
-=======
-4 feature line
->>>>>>> a950f5f95eb04d3ffb35b02c02ffe9925b4a950c
